980e0862d331f2bc03569ce7f48db2dc94a7e51f
fuhaiwei <fuhaiwei@126.com>
2018-07-09 14:40:06 +0800

    add spring template

--- src/main/java/fuhaiwei/bmoe2018/autorun/RunTask.java
+++ src/main/java/fuhaiwei/bmoe2018/autorun/RunTask.java
@@ -3,6 +3,8 @@ package fuhaiwei.bmoe2018.autorun;
 import fuhaiwei.bmoe2018.handler.Handler;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroupFile;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,7 +13,8 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
 
 import static fuhaiwei.bmoe2018.spider.BmoeSpider.fetchCurrent;
 import static fuhaiwei.bmoe2018.spider.BmoeSpider.fetchData;
@@ -22,7 +25,10 @@ public class RunTask {
     public static final DateTimeFormatter DATE_FORMATTER;
     public static final DateTimeFormatter DATE_TIME_FORMATTER;
 
+    private static final STGroupFile ST_GROUP_FILE;
+
     static {
+        ST_GROUP_FILE = new STGroupFile("src/main/java/fuhaiwei/bmoe2018/template/template.stg", '$', '$');
         DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd/HH:mm");
     }
@@ -46,66 +52,40 @@ public class RunTask {
     }
 
     private static String buildHtml(String date) {
-        StringBuilder builder = new StringBuilder();
+        File[] files = new File("output/" + date)
+                .listFiles(pathname -> pathname.isFile() && pathname.getName().endsWith(".txt"));
+        Objects.requireNonNull(files);
+        
+        Arrays.sort(files, Comparator.reverseOrder());
+
+        ST bmoe2018 = ST_GROUP_FILE.getInstanceOf("bmoe2018");
+
+        bmoe2018.add("fileNames", IntStream.range(0, files.length).boxed()
+                .map(index -> buildFileName(files, index)).toArray());
+        bmoe2018.add("fileContents", IntStream.range(0, files.length).boxed()
+                .map(index -> buildFileContent(files, index)).toArray());
+
+        return bmoe2018.render();
+    }
+
+    private static String buildFileName(File[] files, int index) {
+        ST fileName = ST_GROUP_FILE.getInstanceOf("fileName");
+        fileName.add("fileId", "txt" + index);
+        fileName.add("fileName", files[index].getName());
+        fileName.add("active", index == 2);
+        return fileName.render();
+    }
+
+    private static String buildFileContent(File[] files, int index) {
+        ST fileContent = ST_GROUP_FILE.getInstanceOf("fileContent");
+        fileContent.add("fileId", "txt" + index);
         try {
-            List<String> strings = Files.readAllLines(new File("src/main/java/fuhaiwei/bmoe2018/template/template.html").toPath());
-            for (int i = 0; i < 14; i++) {
-                builder.append(strings.get(i)).append("\n");
-            }
-
-            File[] files = new File("output/" + date)
-                    .listFiles(pathname -> pathname.isFile() && pathname.getName().endsWith(".txt"));
-            if (files != null) {
-                Arrays.sort(files, Comparator.reverseOrder());
-            }
-
-            if (files == null || files.length == 0) {
-                builder.append("本日暂无数据");
-            } else {
-                builder.append("<ul id=\"myTab\" class=\"nav nav-tabs\">\n");
-                for (int i = 0; i < files.length; i++) {
-                    appendTab(builder, files, i);
-                }
-                builder.append("</ul>\n");
-
-
-                builder.append("<div id=\"myTabContent\" class=\"tab-content\" style=\"padding: 8px\">\n");
-                for (int i = 0; i < files.length; i++) {
-                    appendContent(builder, files, i);
-                }
-                builder.append("</div>\n");
-            }
-
-            for (int i = 14; i < strings.size(); i++) {
-                builder.append(strings.get(i)).append("\n");
-            }
+            fileContent.add("fileContent", Files.readAllLines(files[index].toPath()));
         } catch (IOException e) {
-            e.printStackTrace();
+            fileContent.add("fileContent", e.getMessage());
         }
-        return builder.toString();
-    }
-
-    private static void appendTab(StringBuilder builder, File[] files, int index) {
-        if (index == 2) {
-            builder.append("   <li class=\"active\">\n");
-        } else {
-            builder.append("   <li>\n");
-        }
-        builder.append("      <a href=\"#txt").append(index).append("\" data-toggle=\"tab\">\n");
-        builder.append("         ").append(files[index].getName()).append("\n");
-        builder.append("      </a>\n");
-        builder.append("   </li>\n");
-    }
-
-    private static void appendContent(StringBuilder builder, File[] files, int index) throws IOException {
-        if (index == 2) {
-            builder.append("   <div class=\"tab-pane fade in active\" id=\"txt2\">\n");
-        } else {
-            builder.append("   <div class=\"tab-pane fade\" id=\"txt").append(index).append("\">\n");
-        }
-        List<String> strings = Files.readAllLines(files[index].toPath());
-        strings.forEach(s -> builder.append("      <p>").append(s).append("</p>\n"));
-        builder.append("   </div>\n");
+        fileContent.add("active", index == 2);
+        return fileContent.render();
     }
 
 }
